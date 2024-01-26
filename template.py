6e37dab1aac8d98df549502d1bf30e36a20f7068
Akhil Bharanidhar <akhilb15@outlook.com>
2024-01-26 03:52:52 +0000

    stuff

--- template.py
+++ template.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# This exploit template was generated via:
-# $ pwn template
 from pwn import *
 
-# Set up pwntools for the correct architecture
-context.update(arch='amd64')
-context.terminal = ['tmux', 'splitw', '-v']
-exe = './path/to/binary'
+
+exe = ELF('./path/to/binary')
 host = 'chal.sigpwny.com'
 port = 1337
 
-# Many built-in settings can be controlled on the command-line and show up
-# in "args".  For example, to dump all data sent/received, and disable ASLR
-# for all created processes...
-# ./exploit.py DEBUG NOASLR
+# libc = ELF('./path/to/libc')
+# rop = ROP(exe)
+
+context.arch = 'amd64'
+context.terminal = ['tmux', 'splitw', '-h']
+context.binary = exe
 
 
 
-def start(argv=[], *a, **kw):
+# Run without randomization
+# python3 solve.py GDB NOASLR
+
+
+
+def conn(argv=[], *a, **kw):
     '''Start the exploit against the target.'''
     if args.GDB:
-        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
     elif args.REMOTE:
         return remote(host, port)
     else:
         return process([exe] + argv, *a, **kw)
 
-# Specify your GDB script here for debugging
-# GDB will be launched if the exploit is run via e.g.
-# ./exploit.py GDB
 gdbscript = '''
 b *main
 '''.format(**locals())
@@ -38,15 +38,20 @@ b *main
 #                    EXPLOIT GOES HERE
 #===========================================================
 
-io = start()
+def solve():
+    io = conn()
 
-# shellcode = asm(shellcraft.sh())
-# payload = fit({
-#     32: 0xdeadbeef,
-#     'iaaa': [1, 2, 'Hello', 3]
-# }, length=128)
-# io.send(payload)
-# flag = io.recv(...)
-# log.success(flag)
+    # shellcode = asm(shellcraft.sh())
+    # payload = fit({
+    #     32: 0xdeadbeef,
+    #     'iaaa': [1, 2, 'Hello', 3]
+    # }, length=128)
+    # io.send(payload)
+    # flag = io.recv(...)
+    # log.success(flag)
 
-io.interactive()
+    io.interactive()
+    
+if __name__ == "__main__":
+    solve()
+
